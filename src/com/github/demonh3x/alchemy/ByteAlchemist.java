9093edfe2cabbc0d6079389c8b82909689945007
mateu <mateu@touchiteasy.com>
2014-10-10 23:19:08 +0200

    Removed unused code

--- src/com/github/demonh3x/alchemy/ByteAlchemist.java
+++ src/com/github/demonh3x/alchemy/ByteAlchemist.java
@@ -1,7 +1,5 @@
 package com.github.demonh3x.alchemy;
 
-import com.github.demonh3x.alchemy.Alchemist;
-
 public class ByteAlchemist implements Alchemist<Byte, byte[]>{
     @Override
     public byte[] transmuteForwards(Byte aByte) {
